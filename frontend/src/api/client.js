0c488659b55da7696b6e515f369d3efb2f71729c
Yashmit <yashmitvaish4454@gmail.com>
2026-03-19 21:33:00 +0530

    fix

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,5 +1,4 @@
-const BASE = 'http://localhost:8000'
-
+const BASE = process.env.REACT_APP_API_BASE || 'http://localhost:8000'
 class ApiError extends Error {
   constructor(status, detail) {
     super(detail)
